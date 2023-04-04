7a09471dac1b2936e80d7254ba6b0a4225c48a47
ardhiarah <ardhiarah@gmail.com>
2023-04-04 10:05:21 +0700

    Change templates

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,17 +4,35 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CV GEN</title>
-    <meta content="CV GEN - ardhiarah" name="keywords">
-    <meta content="Website CV Generator by ardhiarah, untuk mempermudah pembuatan cvmu." name="description">
+    <title>Repo LTJ</title>
+    <meta content="This is repo LTJ ready to fly" name="keywords">
+    <meta content="This is repo LTJ ready to fly. Doesn't need to install all the npm." name="description">
     <link rel="icon" href="/images/a.png" sizes="16x16">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
-<body>
-    <div class="p-20 bg-slate-700 text-white text-center">THIS IS CVGENs</div>
+<body id="body" class="bg-slate-700 text-white text-center transition-all">
+    <div id="repo" class="font-black mt-20 mx-10 px-4 py-2 rounded-lg ring-1 ring-cyan-300 shadow-lg shadow-teal-400 bg-teal-500">
+        THIS is Repo LTJ
+    </div>
+
+    <button id="button" onclick="repo()" class="mt-10 font-black px-4 py-2 rounded-full bg-gradient-to-r from-green-400 to-blue-500 hover:ring-1 hover:ring-cyan-300 hover:shadow-sm hover:shadow-teal-400">Testing Button</button>
+
 </body>
 <script>
+    function repo() {
+        if ($('#repo').text() == 'THIS is Repo LTJ') {
+            $('#repo').html('jQuery Active!');
+            $('#repo').removeClass('ring-cyan-300 shadow-lg shadow-teal-400 bg-teal-500');
+            $('#repo').addClass('ring-yellow-300 shadow-lg shadow-lime-400 bg-lime-500');
+            $('#button').html('Rollback Button');
+        } else {
+            $('#repo').html('THIS is Repo LTJ');
+            $('#repo').removeClass('ring-yellow-300 shadow-lg shadow-lime-400 bg-lime-500');
+            $('#repo').addClass('ring-cyan-300 shadow-lg shadow-teal-400 bg-teal-500');
+            $('#button').html('Testing Button');
+        }
+    }
 
 </script>
 </html>
