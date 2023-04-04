9cfc15d5a9b9f3785c6348cab514b31067682b4b
ardhiarah <ardhiarah@gmail.com>
2023-04-04 09:22:16 +0700

    Master Files

---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CV GEN</title>
+    <meta content="CV GEN - ardhiarah" name="keywords">
+    <meta content="Website CV Generator by ardhiarah, untuk mempermudah pembuatan cvmu." name="description">
+    <link rel="icon" href="/images/a.png" sizes="16x16">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+    <div class="p-20 bg-slate-700 text-white text-center">THIS IS CVGENs</div>
+</body>
+<script>
+
+</script>
+</html>
---
+++ storage/framework/views/0182be6343208927d9f18c38b1a60772714e5733.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CV GEN</title>
+    <meta content="CV GEN - ardhiarah" name="keywords">
+    <meta content="Website CV Generator by ardhiarah, untuk mempermudah pembuatan cvmu." name="description">
+    <link rel="icon" href="/images/a.png" sizes="16x16">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</head>
+<body>
+    <div class="p-20 bg-slate-700 text-white text-center">TESTING</div>
+</body>
+<script>
+
+</script>
+</html>
+<?php /**PATH C:\laragon\www\cvgen\resources\views/master.blade.php ENDPATH**/ ?>
